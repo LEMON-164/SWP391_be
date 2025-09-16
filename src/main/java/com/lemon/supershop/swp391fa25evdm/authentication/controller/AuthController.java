d1b261ac980ae583bfabda16a5c1bd852cc19a61
Lemon-164 <minhlevu1642003@gmail.com>
2025-09-16 09:41:50 +0700

    remove cart

--- src/main/java/com/lemon/supershop/swp391fa25evdm/authentication/controller/AuthController.java
+++ src/main/java/com/lemon/supershop/swp391fa25evdm/authentication/controller/AuthController.java
@@ -1,4 +1,7 @@
 package com.lemon.supershop.swp391fa25evdm.authentication.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
 }
--- src/main/java/com/lemon/supershop/swp391fa25evdm/authentication/repository/AuthRepo.java
+++
@@ -1,7 +0,0 @@
-package com.lemon.supershop.swp391fa25evdm.authentication.repository;
-
-import com.lemon.supershop.swp391fa25evdm.user.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthRepo extends JpaRepository<User, Long> {
-}
