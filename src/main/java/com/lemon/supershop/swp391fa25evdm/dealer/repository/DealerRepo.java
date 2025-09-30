6dcb0ac38a5ffc9239f19660c44724361bda109b
NQHP <hoangphapahk@gmail.com>
2025-09-30 11:11:06 +0700

    Add TestDrive API module and fix repository issues

--- src/main/java/com/lemon/supershop/swp391fa25evdm/dealer/repository/DealerRepo.java
+++ src/main/java/com/lemon/supershop/swp391fa25evdm/dealer/repository/DealerRepo.java
@@ -1,11 +1,12 @@
 package com.lemon.supershop.swp391fa25evdm.dealer.repository;
 
-import com.lemon.supershop.swp391fa25evdm.dealer.model.entity.Dealer;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import com.lemon.supershop.swp391fa25evdm.dealer.model.entity.Dealer;
 @Repository
 public interface DealerRepo extends JpaRepository<Dealer, Integer> {
 
